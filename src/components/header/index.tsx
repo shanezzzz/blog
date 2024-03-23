0661ab5c35ca996bb9e63aa60cc1bb2e75afcbd3
shanezzzz <shanezeng77@gmail.com>
2024-03-24 02:02:03 +0800

    feat: 🎸 add chakra icon&change ui

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Drawer,
+  DrawerContent,
+  DrawerBody,
+  DrawerOverlay,
+  useDisclosure,
+  DrawerCloseButton,
+  Box,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/navigation";
 
 export default function Header() {
   const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <header className="sticky top-0 z-10 bg-neutral-100">
@@ -13,6 +24,12 @@ export default function Header() {
         >
           SHANE STUDIO
         </a>
+        <div
+          className="h-8 w-8 cursor-pointer text-3xl md:hidden"
+          onClick={onOpen}
+        >
+          <HamburgerIcon />
+        </div>
         <nav className="hidden space-x-8 text-xl md:block">
           <Button variant="link" onClick={() => router.replace("/")}>
             Home
@@ -25,6 +42,28 @@ export default function Header() {
           </Button>
         </nav>
       </section>
+      <Drawer placement="top" onClose={onClose} isOpen={isOpen} isFullHeight>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody
+            onClick={onClose}
+            className="flex justify-center items-center"
+          >
+            <div className="flex flex-col h-1/3 justify-around items-center">
+              <Button variant="link" onClick={() => router.replace("/")}>
+                Home
+              </Button>
+              <Button variant="link" onClick={() => router.push("/blog")}>
+                Blog
+              </Button>
+              <Button variant="link" onClick={() => router.replace("/about")}>
+                About
+              </Button>
+            </div>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </header>
   );
 }
