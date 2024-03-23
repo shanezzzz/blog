e895e704b9895eea3dac0dc6c2cb2e352aa45921
shanezzzz <shanezeng77@gmail.com>
2024-03-23 18:18:21 +0800

    feat: 🎸 add header component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,16 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
+import Header from "@/components/header";
 
 const theme = extendTheme({
+  components: {
+    Button: {
+      defaultProps: {
+        colorScheme: "#334155",
+      },
+    },
+  },
   styles: {
     global: {
       body: {
@@ -15,5 +23,10 @@ const theme = extendTheme({
 });
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Header />
+      {children}
+    </ChakraProvider>
+  );
 }
