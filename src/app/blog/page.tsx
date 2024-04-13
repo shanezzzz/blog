e6c719ab67356af018135082a6f72875f55d88bf
shanezzzz <shanezeng77@gmail.com>
2024-04-13 17:08:05 +0800

    feat: 🎸 add new article

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardBody, Text, Image, Stack, Heading } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const defaultArticleData = {
@@ -9,11 +9,34 @@ const defaultArticleData = {
   title: "Hi, nice to meet you! 👋",
   context:
     "I am Shane, and I am glad you have come to browse my personal website. In the future, I will be posting my products and articles here.",
+  id: "1",
+};
+
+const ArticleData1 = {
+  imageUrl:
+    "https://www.notion.so/images/page-cover/rijksmuseum_jansz_1637.jpg",
+  title: "GO THE SEA",
+  context: "-What’s the GTS? -For Freedom and Compound interest",
+  id: "2",
 };
 
 export default function Blog() {
   const router = useRouter();
   const [articleList, setArticleList] = useState([defaultArticleData]);
+
+  const handleGoDetails = (id: string) => {
+    if (id === "2") {
+      router.push(
+        "https://pie-bassoon-564.notion.site/GTS-0865aea1001c4f9ea2d70cf1add6223f?pvs=4"
+      );
+    } else {
+      router.push(`/blog/${id}`);
+    }
+  };
+
+  useEffect(() => {
+    setArticleList((prev) => prev.concat(ArticleData1));
+  }, []);
 
   return (
     <main className="pb-32">
@@ -29,10 +52,15 @@ export default function Blog() {
             <Card
               key={item.title}
               className="cursor-pointer"
-              onClick={() => router.push("/blog/1")}
+              onClick={() => handleGoDetails(item.id)}
             >
               <CardBody>
-                <Image src={item.imageUrl} alt="blogImage" borderRadius="lg" />
+                <Image
+                  src={item.imageUrl}
+                  style={{ width: 568, height: 568, objectFit: "cover" }}
+                  alt="blogImage"
+                  borderRadius="lg"
+                />
                 <Stack mt="6" spacing="3">
                   <Heading size="md">{item.title}</Heading>
                   <Text>{item.context}</Text>
